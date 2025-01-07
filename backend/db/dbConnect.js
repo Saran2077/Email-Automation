7aa683797aae58831b49d8f96239fc9ec1cf4ac4
Saran M <saran.m@adya.ai>
2025-01-07 22:54:29 +0530

    Refactor MongoDB connection in dbConnect.js to simplify connection options
    - Removed unnecessary options from mongoose.connect() for cleaner code.
    - Enhanced error handling by removing commented-out throw statement.
    
    This change streamlines the database connection logic while maintaining functionality.

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -2,13 +2,9 @@ import mongoose from 'mongoose';
 
 export async function connectToMongoDB(uri) {
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log("Connected to MongoDB with Mongoose");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
-        // throw error;
     }
 }
