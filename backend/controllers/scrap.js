cccdbb64882b73f7f9ef57be43c25bcda2014bd4
Saran M <saran.m@adya.ai>
2025-01-08 17:38:15 +0530

    changes

--- backend/controllers/scrap.js
+++ backend/controllers/scrap.js
@@ -73,7 +73,7 @@ const fetchAllCompanies = async (req, res) => {
 
     const companiesList = await fetchCompaniesList(filters);
     console.log('companiesList', JSON.stringify(companiesList?.result?.[0]))
-    for (const company of companiesList?.result) {
+    for (const company of companiesList?.result || []) {
         const processedData = extractRequiredFields(company);
         allCompanyData.push(processedData);
     }
